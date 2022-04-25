54f812ade9f6c6e2be6f7f9b2f2b0b117e20a060
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-04-25 17:01:32 -0300

    Resolvendo problema de sincronismo com useEffect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { FaFacebook } from 'react-icons/fa';
 import { AiFillInstagram, AiFillYoutube, AiFillDelete } from 'react-icons/ai';
 import StarwarsContext from '../context/StarwarsContext';
 import './HeaderStyle.css';
 
+const filterColumns = [
+  'population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water',
+];
 function Header() {
-  const filterColumns = [
-    'population',
-    'orbital_period',
-    'diameter',
-    'rotation_period',
-    'surface_water',
-  ];
   const {
     data,
     isLoading,
@@ -27,14 +27,19 @@ function Header() {
     filteredPlanets,
     filterByNumericValues,
     setFilters } = useContext(StarwarsContext);
+
   const [col, setCol] = useState(filterColumns);
-  /*  const [isFilter, setIsFilter] = useState(true); */
-  const removeColumns = () => {
+  /*   const removeColumns = () => {
     const c = filterColumns.filter((elem) => !filterByNumericValues
       .find((f) => f.column === elem));
-    console.log(c);
     setCol(c);
-  };
+  }; */
+
+  useEffect(() => {
+    const c = filterColumns.filter((elem) => !filterByNumericValues
+      .find((f) => f.column === elem));
+    setCol(c);
+  }, [filterByNumericValues]);
 
   const bttnFilter = () => {
     setFilters([
@@ -44,7 +49,8 @@ function Header() {
         comparison: comparisonFilter,
         value: valueFilter,
       }]);
-
+    /* removeColumns(); */
+    console.log(filterByNumericValues);
     if (comparisonFilter === 'maior que') {
       const arrComparisonBig = filteredPlanets
         .filter((elem) => (Number(elem[columnFilter]) > Number(valueFilter)));
@@ -60,8 +66,6 @@ function Header() {
         .filter((elem) => (Number(elem[columnFilter]) === Number(valueFilter)));
       setFilteredPlanets(arrComparisonEqual);
     }
-    console.log(filterByNumericValues);
-    removeColumns();
   };
 
   const removeFilters = () => {
@@ -70,6 +74,7 @@ function Header() {
     setComparisonFilter('maior que');
     setValueFilter('0');
     setCol(filterColumns);
+    setFilters([]);
   };
 
   return (
@@ -78,70 +83,78 @@ function Header() {
         <div>
           <div className="header--container">
             <div className="header--nav">
-              <a href="https://www.facebook.com/StarWars.br/?brand_redir=169299103121699" className="header--icons">
+              <a href="https://www.facebook.com/StarWars.br" className="header--icons" target="_blank" rel="noreferrer" alt="logo-facebook">
                 <FaFacebook size={ 30 } />
               </a>
-              <a href="https://www.instagram.com/starwars/" className="header--icons">
+              <a href="https://www.instagram.com/starwars/" className="header--icons" target="_blank" rel="noreferrer" alt="logo-instagram">
                 <AiFillInstagram size={ 30 } />
               </a>
-              <a href="https://www.youtube.com/user/starwars" className="header--icons">
+              <a href="https://www.youtube.com/user/starwars" className="header--icons" target="_blank" rel="noreferrer" alt="logo-youtube">
                 <AiFillYoutube size={ 30 } />
               </a>
             </div>
             <div className="logo">
               <img src="https://www.freepnglogos.com/uploads/lego-png-logo/star-wars-png-logo-16.png" alt="logo starwars" />
             </div>
-            <input
-              data-testid="name-filter"
-              type="text"
-              placeholder="Let's find a planet! 🔍"
-              value={ searchInput }
-              onChange={ (e) => setSearchInput(e.target.value) }
-            />
+            <label htmlFor="name-filter">
+              <input
+                data-testid="name-filter"
+                type="text"
+                placeholder="Let's find a planet! 🔍"
+                value={ searchInput }
+                onChange={ (e) => setSearchInput(e.target.value) }
+              />
+            </label>
           </div>
           <div className="filters--container">
-            <select
-              data-testid="column-filter"
-              value={ columnFilter }
-              onChange={ (e) => {
-                setColumFilter(e.target.value);
-              /* ; setFilters([
+            <label htmlFor="column-filter">
+              <select
+                data-testid="column-filter"
+                value={ columnFilter }
+                onChange={ (e) => {
+                  setColumFilter(e.target.value);
+                  /* ; setFilters([
                 ...filterByNumericValues,
                 {
                   column: e.target.value,
                 }]); */
-              } }
-            >
-              {col.map((option) => (
-                <option
-                  key={ option }
-                  value={ option }
-                >
-                  { option }
+                } }
+              >
+                {col.map((option) => (
+                  <option
+                    key={ option }
+                    value={ option }
+                  >
+                    { option }
+                  </option>
+                ))}
+              </select>
+            </label>
+            <label htmlFor="comparison-filter">
+              <select
+                data-testid="comparison-filter"
+                value={ comparisonFilter }
+                onChange={ (e) => setComparisonFilter(e.target.value) }
+              >
+                <option value="maior que">
+                  maior que
                 </option>
-              ))}
-            </select>
-            <select
-              data-testid="comparison-filter"
-              value={ comparisonFilter }
-              onChange={ (e) => setComparisonFilter(e.target.value) }
-            >
-              <option value="maior que">
-                maior que
-              </option>
-              <option value="menor que">
-                menor que
-              </option>
-              <option value="igual a">
-                igual a
-              </option>
-            </select>
-            <input
-              type="number"
-              data-testid="value-filter"
-              value={ valueFilter }
-              onChange={ (e) => setValueFilter(e.target.value) }
-            />
+                <option value="menor que">
+                  menor que
+                </option>
+                <option value="igual a">
+                  igual a
+                </option>
+              </select>
+            </label>
+            <label htmlFor="value-filter">
+              <input
+                type="number"
+                data-testid="value-filter"
+                value={ valueFilter }
+                onChange={ (e) => setValueFilter(e.target.value) }
+              />
+            </label>
             <button
               type="button"
               data-testid="button-filter"
